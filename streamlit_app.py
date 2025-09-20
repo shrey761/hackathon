1748b4029a656032f6f090282b032f3c2068b6cd
shrey761 <shreyapalan4@gmail.com>
2025-09-20 23:36:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,14 @@ from app.jd_parser import parse_jd
 from app.scoring import compute_similarity
 from app.feedback import generate_feedback
 from app.database import save_result, load_results
+import spacy
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 
 
